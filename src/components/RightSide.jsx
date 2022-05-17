47ec96b4a0120077e23c37cdce977343f702fd76
organicien <bouraoui.salah@yahoo.fr>
2022-05-17 15:26:04 +0100

    first commit

---
+++ src/components/RightSide.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Image} from 'react-bootstrap';
+
+const RightSide = () => {
+    return (
+        <div>
+            <Image src="./img/interview.jpg" thumbnail style={{border:"none"}}/>
+        </div>
+    )
+}
+
+export default RightSide;
